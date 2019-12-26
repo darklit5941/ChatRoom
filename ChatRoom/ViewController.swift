80fe387b7fe48fb91d87688e4df5aa7dd59d5273
DavidHua <darklit@msn.com>
2019-12-26 20:23:08 +0800

    Initial Commit

---
+++ ChatRoom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatRoom
+//
+//  Created by Hua David on 2019/12/26.
+//  Copyright © 2019 darklit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
