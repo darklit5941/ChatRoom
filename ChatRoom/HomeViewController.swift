ccc4cdeaf2ecd0f39cc9b02c8adcf1d81fbda2be
DavidHua <darklit@msn.com>
2020-01-06 22:38:28 +0800

    製作第二個table view

--- ChatRoom/HomeViewController.swift
+++ ChatRoom/HomeViewController.swift
@@ -8,6 +8,26 @@
 
 import UIKit
 
+struct frend {
+    let frend_id: Int
+    let name: String
+    let nick_name: String
+    let picture: String
+    let description: String
+}
+
+struct homeList {
+    let frends: [frend]
+    let chats: [chat]
+}
+
+var homeLists =
+    homeList(frends: [
+        frend(frend_id: 1, name: "獅子座", nick_name: "獅獅", picture: "leo", description: "森林之王")
+    ], chats: [
+        chat(chat_id: 1, name: "風象星座討論", picture: "", last_message: "是不是都很瘋?", last_message_time: "12/04")
+    ])
+
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
@@ -35,19 +55,57 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    //每個section裡的row個數
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chats.count
+        if section == 0 {
+            return homeLists.frends.count
+        }else{
+            return homeLists.chats.count
+        }
     }
     
+    //有幾個section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    //section內容
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "好友"
+        case 1:
+            return "群組"
+        default:
+            return nil
+        }
+    }
+    
+    //row內容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MessageListTableViewCell
-        let data = chats[indexPath.row]
-        cell.avatarUIImageView.image = UIImage(named: data.picture)
-        cell.nameLabel.text = data.name
-        cell.statusLabel.text = data.last_message
-        cell.DateLabel.text = data.last_message_time
+        if indexPath.section == 0 {
+            let data = homeLists.frends[indexPath.row]
+            cell.avatarUIImageView.image = UIImage(named: self.checkImage(pictureName: data.picture))
+            cell.nameLabel.text = data.name
+            cell.statusLabel.text = data.description
+            cell.DateLabel.text = ""
+        }else{
+            let data = homeLists.chats[indexPath.row]
+            cell.avatarUIImageView.image = UIImage(named: self.checkImage(pictureName: data.picture))
+            cell.nameLabel.text = data.name
+            cell.statusLabel.text = data.last_message
+            cell.DateLabel.text = data.last_message_time
+        }
+        
         return cell
     }
     
-    
+    func checkImage(pictureName: String) -> String {
+        if pictureName.isEmpty {
+            return "user-default"
+        }else{
+            return pictureName
+        }
+    }
 }
