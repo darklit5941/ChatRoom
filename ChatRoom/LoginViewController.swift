bfe1c90973d67a2017584226b506c155b1c382e7
DavidHua <darklit@msn.com>
2020-01-16 00:05:18 +0800

    新增加登入的storyboard

---
+++ ChatRoom/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  ChatRoom
+//
+//  Created by Hua David on 2020/1/15.
+//  Copyright © 2020 darklit. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
